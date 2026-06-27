1cc135df86a7e0b198796d8767dbe4b3a5331134
CodieMisiani <nimrodmisiani1@gmail.com>
2026-06-27 19:19:45 +0300

    feat(routing): register portfolio route

--- client/src/routes/Routes.tsx
+++ client/src/routes/Routes.tsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import HomePage from "../pages/HomePage";
+import PortfolioPage from "../pages/PortfolioPage";
 
 export default function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
+      <Route path="/portfolio" element={<PortfolioPage />} />
       <Route path="*" element={<HomePage />} />
     </Routes>
   );
