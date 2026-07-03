26a50f7e877e8472a1b8781e528475cb20123384
CodieMisiani <nimrodmisiani1@gmail.com>
2026-07-03 04:18:29 +0300

    chore: prepare for kenya localization fixes

--- server/src/lib/validators/phone.ts
+++ server/src/lib/validators/phone.ts
@@ -1,10 +1,11 @@
 import { z } from "zod";
-import { LOCALE } from "../config/locale.js";
+
+const KENYA_PHONE_REGEX = /^(?:\+254|0)[17]\d{8}$/;
 
 export const kenyanPhone = z
   .string()
   .min(9)
   .max(14)
-  .regex(LOCALE.phoneRegex, "invalid Kenyan phone number");
+  .regex(KENYA_PHONE_REGEX, "invalid Kenyan phone number");
 
 export default kenyanPhone;
